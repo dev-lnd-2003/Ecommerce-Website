01d63e3af5da46606ff64ba0da91c7113c7422db
Shazaza <nhatduy02.tphcm@gmail.com>
2023-01-28 15:26:28 +0700

    add carousel of owl in homepage

---
+++ js/homepageOwl.js
@@ -0,0 +1,123 @@
+
+
+$('#product-new').owlCarousel({
+
+    margin: 10,
+    nav: false,
+    dots: true,
+
+    responsive: {
+        0: {
+            items: 1
+
+        },
+        600: {
+            items: 1
+        },
+
+        1000: {
+            items: 2
+        },
+        1200: {
+            items: 1,
+
+        }
+    }
+})
+
+$('#product-trending').owlCarousel({
+
+    margin: 10,
+    nav: false,
+    dots: true,
+
+    responsive: {
+        0: {
+            items: 1
+
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        },
+        1200: {
+            items: 1,
+
+        }
+    }
+})
+
+$('#product-rated').owlCarousel({
+
+    margin: 10,
+    nav: false,
+    dots: true,
+
+    responsive: {
+        0: {
+            items: 1
+
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        },
+        1200: {
+            items: 1,
+
+        }
+    }
+})
+
+$('#product-featured').owlCarousel({
+
+    margin: 10,
+    nav: false,
+    dots: false,
+
+    responsive: {
+        0: {
+            items: 1
+
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        },
+        1200: {
+            items: 1,
+
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+
+    margin: 10,
+    nav: false,
+    dots: false,
+
+    responsive: {
+        0: {
+            items: 1
+
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        },
+        1200: {
+            items: 4,
+
+        }
+    }
+})
+
